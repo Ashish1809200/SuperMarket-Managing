2454320816f068038903e6cc5642a4ed6b586f3c
Ashish1809200 <ashishkashyap024@gmail.com>
2024-07-25 20:27:37 +0530

    Added new project

---
+++ WebApplication/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = Categoriesdbrepositories.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            var category = Categoriesdbrepositories.GetCategoryById(id.HasValue ? id.Value : 0);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                Categoriesdbrepositories.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid) 
+            {
+                Categoriesdbrepositories.AddCategory(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int categoryId) 
+        {
+           Categoriesdbrepositories.DeleteCategory(categoryId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
